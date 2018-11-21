bfad5364bfe78bcd031028a353ee58d33a8b0aec
PlainWaterK <664060402@qq.com>
2018-11-21 04:30:32 -0800

    Hello World vscode
    use vscode write hello world first time

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        String str=";";
+        str=str.replace(";","A");
+        System.out.println(str + "HelloWorld");
+    }
+}
