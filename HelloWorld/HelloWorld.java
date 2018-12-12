2d9bc2c9aab97611122783e1636abd88b51082e3
PlainWaterK <664060402@qq.com>
2018-12-11 17:54:52 -0800

    delete some projects

--- HelloWorld/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld{
-    public static void main(String[] args){
-        String str=";";
-        str=str.replace(";","A");
-        System.out.println(str + "HelloWorld");
-    }
-}
